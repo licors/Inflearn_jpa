6252a385613889937b9705c3d1cb869950f5793a
ahn_mac <licors214@gmail.com>
2023-01-11 23:58:37 +0900

    상품 도메인 구현

--- src/main/java/jpastudy/jpashop/domain/item/Item.java
+++ src/main/java/jpastudy/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpastudy.jpashop.domain.item;
 
 import jpastudy.jpashop.domain.Category;
+import jpastudy.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,20 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    /**
+     * 비지니스 로직
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    
+    public void removeStock(int quantity) {
+        int restStock = stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
